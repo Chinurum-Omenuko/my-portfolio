5d9329df38c0a45ffa9bb7644147cfdc949f1653
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-23 01:10:02 -0500

    Creates HomeLanding.tsx component

---
+++ app/components/HomeLanding/HomeLanding.tsx
@@ -0,0 +1,9 @@
+export default function HomeLanding() {
+    return(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex place-items-center relative top-48">
+            <h1 className="text-7xl">My Portfolio</h1>
+        </div>
+    </main>
+    )
+}
