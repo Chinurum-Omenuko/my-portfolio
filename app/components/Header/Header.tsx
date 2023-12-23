d69fbce252f95589431e9563f72d65dfd8e96364
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-23 00:38:47 -0500

    Edits return line

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,23 +1,24 @@
+import './HeaderStyle.css'
+
 export default function Header() {
-    return
-    (
-        <header>
-            <nav>
-                <div className="logo">
-                    <a href="/">Chinurum Omenuko</a>
-                </div>
+    return (
+    <header>
+        <nav>
+            <div className="logo">
+                <a href="/">Chinurum Omenuko</a>
+            </div>
 
-                <div className="nav-items">
-                    <a href="">About Me</a>
-                    <a href="">Projects</a>
-                    <a href="">Resume</a>
-                    <a href="">Contacts</a>
-                </div>
+            <div className="nav-items">
+                <a href="">About Me</a>
+                <a href="">Projects</a>
+                <a href="">Resume</a>
+                <a href="">Contacts</a>
+            </div>
 
-                <div className="github">
-                    <a href="https://github.com/Chinurum-Omenuko">My Github</a>
-                </div>
-            </nav>
-        </header>
-    )
+            <div className="github">
+                <button><a href="https://github.com/Chinurum-Omenuko">My Github</a></button>
+            </div>
+        </nav>
+    </header>
+    );
 }
