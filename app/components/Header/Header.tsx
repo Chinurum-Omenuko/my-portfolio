ea8bbd3f7f1a570a16cd047ee45999ceb02449a6
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-22 23:18:54 -0500

    Creates Header.tsx header in header branch

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+export default function Header() {
+    return
+    (
+        <header>
+            <nav>
+                <div className="logo">
+                    <a href="/">Chinurum Omenuko</a>
+                </div>
+
+                <div className="nav-items">
+                    <a href="">About Me</a>
+                    <a href="">Projects</a>
+                    <a href="">Resume</a>
+                    <a href="">Contacts</a>
+                </div>
+
+                <div className="github">
+                    <a href="https://github.com/Chinurum-Omenuko">My Github</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
