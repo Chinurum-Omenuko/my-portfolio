e735507ea163597cb02af008a1737fd3256a94ef
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-24 19:52:49 -0500

    makes contacts page. Change font soon

---
+++ app/components/Contacts/Contacts.tsx
@@ -0,0 +1,23 @@
+export default function Contacts() {
+    return(
+        <section className="h-screen bg-black grid place-items-center">
+            <div className="contacts-container h-4/5 w-4/5 grid text-center">
+                <div className="title">
+                    <h2>Contacts</h2>
+                </div>
+
+                <div className="contacts">
+                    <div>
+                        <img src="" alt="" />
+                        <a href="">chinuomenu@gmail.com</a>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <a href="https://www.linkedin.com/in/chinurum-omenuko-612a161a3/">Chinurum Omenuko</a>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    )
+}   
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Header from './components/Header/Header'
 import HomeLanding from './components/HomeLanding/HomeLanding'
+import Contacts from './components/Contacts/Contacts'
 
 export default function Home() {
   return (
     <div className="pagecontainer">
       <Header />
       <HomeLanding />
+      <Contacts />
     </div>
 
   )
