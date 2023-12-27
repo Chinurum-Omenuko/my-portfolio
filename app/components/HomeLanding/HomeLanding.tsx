747ed0af2eb56f492a82b9e87fb7386f1e1c1a03
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-27 01:54:43 -0500

    Changes Z-index of H1

--- app/components/HomeLanding/HomeLanding.tsx
+++ app/components/HomeLanding/HomeLanding.tsx
@@ -2,7 +2,7 @@ export default function HomeLanding() {
     return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="flex place-items-center relative top-48">
-            <h1 className="text-7xl">My Portfolio</h1>
+            <h1 className="text-7xl z-10">My Portfolio</h1>
         </div>
     </main>
     )
