152e2898e8b46ddb0c4d363c6e9c977c10ccc1f4
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-27 19:04:50 -0500

    Creates Resume.tsx and adds it to page.tsx

---
+++ app/components/Resume/Resume.tsx
@@ -0,0 +1,33 @@
+export default function Resume() {
+    return(
+        <section className="h-screen bg-black grid place-items-center">
+            <div className="contacts-container h-4/5 w-4/5 grid grid-rows-3 text-center">
+                <div className="title">
+                    <h2>Resume</h2>
+                </div>
+
+                <div className="competencies flex flex-row justify-center">
+                    <li><a href="">Frontend</a></li>
+                    <li><a href="">Backend</a></li>
+                    <li><a href="">DevOps</a></li>
+                    <li><a href="">Experience</a></li>
+                </div>
+
+                <div className="cv">
+                    <div>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <a href="https://www.linkedin.com/in/chinurum-omenuko-612a161a3/">Chinurum Omenuko</a>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    )
+}   
