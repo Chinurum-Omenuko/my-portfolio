cfd7e052ef16da548e5adffba6668c93d20e3acb
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-28 00:14:34 -0500

    adds about section component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Header from './components/Header/Header'
 import HomeLanding from './components/HomeLanding/HomeLanding'
 import Contacts from './components/Contacts/Contacts'
 import Resume from './components/Resume/Resume'
+import About from './components/About/About'
 
 export default function Home() {
   return (
     <div className="pagecontainer">
       <Header />
       <HomeLanding />
+      <About />
       <Resume />
       <Contacts />
     </div>
