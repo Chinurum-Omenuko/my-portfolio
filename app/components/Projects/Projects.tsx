40132c70c4a66cbfec1c8929aa1bd835a84e84a3
chinurum-omenuko <omen0003@algonquinlive.com>
2024-01-11 14:47:20 -0500

    changes img tags to nextjs Image tags

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -1,30 +1,34 @@
 import './Projects.css'
+import Image from 'next/image'
 
 export default function Projects() {
     return(
-        
-        <div id="projects" className="grid grid-cols-2 md:grid-cols-3 gap-4 bg-black">
-            <div>
-                <a href="https://infeu.vercel.app/"><img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/51951/forest-fire-fire-smoke-conservation-51951.jpeg?cs=srgb&dl=pexels-pixabay-51951.jpg&fm=jpg&_gl=1*1c2xu9l*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MjE0NC4wLjAuMA.." alt="" /></a>
-                <p>Infeu: Fire tracker</p>
-            </div>
-            <div>
-                <a href="https://onpoint-drivers.vercel.app/"><img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/13861/IMG_3496bfree.jpg?cs=srgb&dl=pexels-jeshootscom-13861.jpg&fm=jpg&_gl=1*1r2momg*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MjA2OC4wLjAuMA.." alt="" /></a>
-                <p>On Point Drivers Driving school</p>
-            </div>
-            <div>
-                <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" />
-            </div>
-            <div>
-                <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" />
-            </div>
-            <div>
-                <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" />
-            </div>
-            <div>
-                <img className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" />
-            </div>
-            
+    <div id="projects" className="grid grid-cols-2 md:grid-cols-3 gap-4 bg-black">
+        <div>
+            <a href="https://infeu.vercel.app/">
+                <Image className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/51951/forest-fire-fire-smoke-conservation-51951.jpeg?cs=srgb&dl=pexels-pixabay-51951.jpg&fm=jpg&_gl=1*1c2xu9l*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MjE0NC4wLjAuMA.." alt="" width={500} height={300} />
+            </a>
+            <p>Infeu: Fire tracker</p>
         </div>
+        <div>
+            <a href="https://onpoint-drivers.vercel.app/">
+                <Image className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/13861/IMG_3496bfree.jpg?cs=srgb&dl=pexels-jeshootscom-13861.jpg&fm=jpg&_gl=1*1r2momg*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MjA2OC4wLjAuMA.." alt="" width={500} height={300} />
+            </a>
+            <p>On Point Drivers Driving school</p>
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" width={500} height={300} />
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" width={500} height={300} />
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" width={500} height={300} />
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://images.pexels.com/photos/4271927/pexels-photo-4271927.jpeg?cs=srgb&dl=pexels-alleksana-4271927.jpg&fm=jpg&_gl=1*xqyztl*_ga*NzgyNDIzMjAxLjE3MDQ4ODE4Njc.*_ga_8JE65Q40S6*MTcwNDg4MTg2Ny4xLjEuMTcwNDg4MTkxMC4wLjAuMA.." alt="" width={500} height={300} />
+        </div>
+    </div>
+
     )
 }
