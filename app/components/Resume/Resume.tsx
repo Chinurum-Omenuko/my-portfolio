4dc996042637b86a133d4c111edbf0632fae3453
chinurum-omenuko <omen0003@algonquinlive.com>
2024-01-11 14:15:00 -0500

    edits reume section

--- app/components/Resume/Resume.tsx
+++ app/components/Resume/Resume.tsx
@@ -23,11 +23,10 @@ export default function Resume() {
                     <div className="skills">
                         <h2 className="mb-5">Backend skills</h2>
                         <ul className="leading-10 text-justify skills-list">
-                            <li>Creating components for single page applications using React and Svelte</li>
-                            <li>Using javascript to make http request to various API Endpoints</li>
-                            <li>Ensuring type safety, testing and debugging using typescript.</li>
-                            <li>TailwindCSS + DaisyUI</li>
-                            <li>Figma</li>
+                            <li>Using Python's Django Framework and Node.Js's Express.js to setup and configure a backend server</li>
+                            <li>Creating Routes for each endpoint</li>
+                            <li>Use and Understand of Object Oriented Design patterns such as Dependency Injection.</li>
+                            <li>Understanding of HTTPS, CORS, API Headers and proficient use of Browser Developer Tools for debugging.</li>
                         </ul>
                         
                     </div>
