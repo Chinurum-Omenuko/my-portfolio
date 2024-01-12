2e14c8d1d662379f1851c77d4c46b78dde5c5cc6
chinurum-omenuko <omen0003@algonquinlive.com>
2024-01-12 16:33:52 -0500

    changes image loading

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://ibb.co/VqmVhsk'],
+        domains: ['images.pexels.com', 'ibb.co'],
     }
 }
 
