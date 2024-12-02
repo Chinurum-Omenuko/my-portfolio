e9776c362941a746651b720806c1b92937506880
Chinurum-Omenuko <omen0003@algonquinlive.com>
2024-12-02 15:07:38 -0500

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header'
 import HomeLanding from './components/HomeLanding/HomeLanding'
 import Contacts from './components/Contacts/Contacts'
 import Resume from './components/Resume/Resume'
-import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Title from './components/Projects/Title'
 
